985f0bbb959c86a500851f023a57ef5c425f13fc
missingdays <daysaremissing@gmail.com>
2015-03-03 19:44:43 +0300

    Add greatest common divisor closes #26

---
+++ main/algorithms/math/gcd/python/gcd.py
@@ -0,0 +1,13 @@
+#Euclidian algorithm
+#Finds greatest common division
+def gcd(a,b):
+    while a!=0 and b!=0:
+        if a > b:
+            a = a % b
+        else:
+            b = b % a
+    return a + b
+
+print(gcd(20, 30)) #10
+print(gcd(15, 30)) #15
+print(gcd(480, 564)) #12
