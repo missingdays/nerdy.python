e2142d20faaf6ab13b7fb896318b85498d96df6a
missingdays <daysaremissing@gmail.com>
2015-03-03 08:35:03 +0300

    Get rid of extra iteration

--- main/algorithms/math/fibonacci/python/fibonacci.py
+++ main/algorithms/math/fibonacci/python/fibonacci.py
@@ -1,10 +1,14 @@
 def fibonacci(n):
+    
+    if n == 1:
+        return 0
+
     a,b = 0, 1 #First two numbers
 
-    for i in range(2, n+1):
+    for i in range(2, n):
         a,b = b, a + b
 
-    return a
+    return b
 
 for i in range(1, 10):
     print(fibonacci(i))
