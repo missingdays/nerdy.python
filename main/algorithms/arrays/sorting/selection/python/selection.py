10c4a237aa6533f71ea2be7cc6c920a22be591fb
missingdays <daysaremissing@gmail.com>
2015-03-05 00:27:46 +0300

    Move ALL python files to main

---
+++ main/algorithms/arrays/sorting/selection/python/selection.py
@@ -0,0 +1,14 @@
+A = [10, 2, 4, 5, 6, -3, 4, 0, 56]
+
+for i in range(len(A)):
+    minimum = i
+    for k in range(i, len(A)):
+        if(A[k] < A[minimum]):
+            minimum = k
+
+    buf = A[i]
+    A[i] = A[minimum]
+    A[minimum] = buf
+
+for i in range(len(A)):
+    print(A[i])
