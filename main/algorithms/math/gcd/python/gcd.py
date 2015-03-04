0781be4508ade0e25618d996e3e7f0e84769bd86
missingdays <daysaremissing@gmail.com>
2015-03-04 08:36:03 +0300

    Move python files to main

--- main/algorithms/math/gcd/python/gcd.py
+++ main/algorithms/math/gcd/python/gcd.py
@@ -1,11 +1,15 @@
 #Euclidian algorithm
 #Finds greatest common division
 def gcd(a,b):
+
     while a!=0 and b!=0:
+
         if a > b:
             a = a % b
+
         else:
             b = b % a
+            
     return a + b
 
 print(gcd(20, 30)) #10
