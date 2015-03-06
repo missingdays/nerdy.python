ea0f9b5057dae5d4d21675de0c2fd78329d84ab6
missingdays <daysaremissing@gmail.com>
2015-03-06 16:16:34 +0300

    Add insertion sort closes #3

---
+++ main/algorithms/arrays/sorting/insertion/python/insertion.py
@@ -0,0 +1,24 @@
+def insertionSort(ar):
+
+    for i in range(1, len(ar)):
+
+        #Get new element
+        current_value = ar[i]
+        #Get position of this element
+        position = i
+
+        #While new element is bigger than compared value
+        while(position > 0 and ar[position-1] > current_value):
+
+            #Dive this element down
+            ar[position] = ar[position-1]
+            position = position - 1
+
+        #Write element to its new place
+        ar[position] = current_value
+
+ar = [0, 1, 10, 15, 7, 20, 4, 6, 90, 5]
+insertionSort(ar)
+
+for i in range(len(ar)):
+    print(ar[i])
