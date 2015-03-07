dca1c61ff92376c043a69b9f8b7234b4ee670780
missingdays <daysaremissing@gmail.com>
2015-03-07 12:01:10 +0300

    Add factorial function Go

---
+++ main/algorithms/math/factorial/go/factorial.go
@@ -0,0 +1,29 @@
+package main
+
+import "fmt"
+
+func factorial(n int) int{
+
+  if n == 0 || n == 1{
+
+    return 1
+
+  }
+
+  a := 1
+
+  for i := 1; i < n + 1; i++ {
+    a = a * i
+  }
+
+  return a
+
+}
+
+func main() {
+
+  for i := 0; i < 10; i++{
+    fmt.Println(factorial(i))
+  }
+
+}
