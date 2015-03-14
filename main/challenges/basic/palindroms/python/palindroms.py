904805e0c2a4e9fae4368c1058ca158f5847ed55
missingdays <daysaremissing@gmail.com>
2015-03-14 17:12:42 +0300

    Add polindroms solution Python

---
+++ main/challenges/basic/palindroms/python/palindroms.py
@@ -0,0 +1,28 @@
+#Number of test cases
+n = int(input())
+
+#For every test case
+for i in range(n):
+
+    #Set count to 0
+    count = 0
+
+    #Read string to be calculated
+    string = input()
+
+    #For every character in that string
+    #to the middle of the string
+    for c in range(int(len(string) / 2)):
+
+        #Character from the left part of the string
+        left = string[c]
+
+        #Character from the right part of the string
+        right = string[len(string) - 1 - c]
+
+        #Calculate how many operations needs to be done
+        #to turn one character to another
+        count += abs(ord(right) - ord(left))
+
+    #Print sum of all differences
+    print(count)
