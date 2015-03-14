682a78f402a916a5c590e9727e58d39c11fd1e5a
missingdays <daysaremissing@gmail.com>
2015-03-14 16:51:08 +0300

    Add Maximum XOR solution Python

---
+++ main/challenges/basic/maximize_xor/python/maximize_xor.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+def maxXOR(x, y):
+
+    #Set maximum to zero
+    maximum = 0
+
+    #For every number from x to y
+    for i in range(x, y+1):
+
+        #For every number from x to y
+        for k in range(x, y+1):
+
+            #Compare their XOR to temporary maximum
+            #if it's bigger than temporary maximum
+            if i ^ k > maximum:
+
+                #Set maximum to its XOR
+                maximum = i ^ k
+
+    #Return final maximum
+    return maximum
+if __name__ == '__main__':
+
+    #Get first number
+    x = int(input())
+
+    #Get second number
+    y = int(input())
+
+    #Print their maxXOR
+    print(maxXOR(x, y))
