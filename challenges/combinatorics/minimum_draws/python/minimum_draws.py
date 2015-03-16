96c3caf67bc75ece61803eb093056c7b96de7111
missingdays <daysaremissing@gmail.com>
2015-03-16 19:34:21 +0300

    Add basic combinatorics challenge -__-

---
+++ challenges/combinatorics/minimum_draws/python/minimum_draws.py
@@ -0,0 +1,9 @@
+#Get number of test cases
+n = int(input())
+
+#For every test case
+for i in range(n):
+
+    #He needs to pull socks/2 (or number of pairs)
+    #plus one to get at least one pair of one-color socks
+    print(int(input()) + 1)
