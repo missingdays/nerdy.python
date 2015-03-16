e885f5d88070b8253f147436c3af07a89f00a573
missingdays <daysaremissing@gmail.com>
2015-03-16 15:56:23 +0300

    Add chocolate feast solution Python

---
+++ challenges/basic/chocolate_feast/python/chocolate_feast.py
@@ -0,0 +1,16 @@
+T = int(input())
+
+for i in range(T):
+
+    N,C,M = str(input()).split()
+    N = int(N)
+    C = int(C)
+    M = int(M)
+
+    answer = N // C
+    b = N // C
+    while b // M > 0:
+        can_buy = b // M
+        answer += can_buy
+        b = b - can_buy * M + b // M
+    print(int(answer))
