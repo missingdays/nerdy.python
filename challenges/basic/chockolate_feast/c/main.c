5db147a950366937eeeb06a8c406870d8dbac50a
dar-drozdova@yandex.ru <dar-drozdova@yandex.ru>
2015-03-16 17:21:57 +0300

    Chocolate_feast c

---
+++ challenges/basic/chockolate_feast/c/main.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+int main(void)
+{
+
+    int money;
+    int price;
+    int change;
+    int sweets=0;
+    int fantiki=0;
+    int g;
+    scanf("%d %d %d", &money,&price,&change);
+    sweets=money/price;
+    fantiki=sweets;
+    while (fantiki>=change) {
+        g=fantiki/change;
+        sweets+=g;
+        fantiki-=(g*change);
+        fantiki+=g;
+
+    }
+    printf("%d",sweets);
+
+    return 0;
+}
+
