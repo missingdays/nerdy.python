9076f1cbc1aaac8eb78b6f75933728314768661d
missingdays <daysaremissing@gmail.com>
2015-03-17 17:16:52 +0300

    Add euqler 61 task

---
+++ challenges/project_euler/61/python/61.py
@@ -0,0 +1,20 @@
+#Triangle n * (n + 1) / 2
+#Square n * n
+#Pentagon n * (3 * n - 1) / 2
+#Hexagon n * (2 * n - 1)
+#Heptagon n * (5 * n - 3) / 2
+#Octagon n * (3 * n - 2)
+
+def getLast(n):
+
+    return n % 100
+
+def getFirst(n):
+
+    return n // 100
+
+types = []
+
+types = input().split()
+
+types = list(map(int, sizes))
