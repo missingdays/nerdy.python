54e1ebef9dd53e6b65546abfc14eca61840b4279
missingdays <daysaremissing@gmail.com>
2015-03-28 19:54:02 +0300

    Add rus_code_cup homework solution

---
+++ challenges/contests/2015/rus_code_cup/fist_qual/homework/python/homework.py
@@ -0,0 +1,11 @@
+for i in range(int(input())):
+
+    x = int(input())
+    y = int(input())
+    z = int(input())
+
+    if x * y == z:
+        print('Infinity')
+
+    else:
+        print('Finite')
