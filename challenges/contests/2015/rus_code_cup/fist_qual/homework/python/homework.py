e056d80a87496ae2a42b74f9d08f2fa911479936
missingdays <daysaremissing@gmail.com>
2015-03-29 10:30:04 +0300

    Add comments

--- challenges/contests/2015/rus_code_cup/fist_qual/homework/python/homework.py
+++ challenges/contests/2015/rus_code_cup/fist_qual/homework/python/homework.py
@@ -1,11 +1,20 @@
+
+#For every test case
 for i in range(int(input())):
 
+    #Get those numbers
     x = int(input())
     y = int(input())
     z = int(input())
 
+    #If they are equal in 10 notation
+    #they are equal and in bigger notations
     if x * y == z:
         print('Infinity')
-
+        
+    #If they are not equal in 10 notation
+    #they are not equal and in bigger notations
+    #and that means there are only finite number
+    #of notations
     else:
         print('Finite')
