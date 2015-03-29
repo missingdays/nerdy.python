4882b0d282fc225cff3aff9224621707649e8b5c
missingdays <daysaremissing@gmail.com>
2015-03-29 10:30:49 +0300

    Add author

--- challenges/contests/2015/rus_code_cup/fist_qual/homework/python/homework.py
+++ challenges/contests/2015/rus_code_cup/fist_qual/homework/python/homework.py
@@ -1,4 +1,11 @@
+'''
+    type: challenge solution
+    theme: contest
+    sub-theme: russian code cup 2015
+    name: b - homework
+    author of code: Evgeny @missingdays Bovykin
 
+'''
 #For every test case
 for i in range(int(input())):
 
@@ -11,7 +18,7 @@ for i in range(int(input())):
     #they are equal and in bigger notations
     if x * y == z:
         print('Infinity')
-        
+
     #If they are not equal in 10 notation
     #they are not equal and in bigger notations
     #and that means there are only finite number
