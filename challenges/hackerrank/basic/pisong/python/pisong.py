598289750ec4ec5607e6157bfee6576d6010a418
missingdays <daysaremissing@gmail.com>
2015-06-05 14:39:03 +0300

    Add coments

--- challenges/hackerrank/basic/pisong/python/pisong.py
+++ challenges/hackerrank/basic/pisong/python/pisong.py
@@ -6,15 +6,29 @@
 #
 # Distributed under terms of the MIT license.
 
+# Pi string
 pi = "31415926535897932384626433833"
 
+# For every test case
 for i in range(int(input())):
+
+    # Current index of the number we should get
     pi_c = 0
+
+    # Assume input string is the song
     song = True
+
+    # For every word in the input string
     for word in input().split():
+
+        # If length of the word doesn't match current number
         if len(word) != int(pi[pi_c]):
+            # It's not a song!
             song = False
+
+        # Increase number counter
         pi_c += 1
+
     if song:
         print("It's a pi song.")
     else:
