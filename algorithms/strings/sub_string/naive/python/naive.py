263449855821c00982286d42ada37765830c202c
missingdays <daysaremissing@gmail.com>
2015-09-08 18:30:48 +0300

    Add interactivity

--- algorithms/strings/sub_string/naive/python/naive.py
+++ algorithms/strings/sub_string/naive/python/naive.py
@@ -6,9 +6,14 @@
 #
 # Distributed under terms of the MIT license.
 
-"""
+'''
+    type: algorithm
+    theme: string
+    sub-theme: search of sub string
+    name: naive
+    author of code: Evgeny @missingdays Bovykin
 
-"""
+'''
 
 def index(original, sub):
     ori_len = len(original)
@@ -26,3 +31,5 @@ def index(original, sub):
 print(index("hello", "l"))
 print(index("hello world", "world"))
 print(index("hello world", "wrld"))
+i = raw_input("now you > ").split()
+print(index(i[0], i[1]))
