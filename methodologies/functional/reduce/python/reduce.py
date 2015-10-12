053ef9e620fc98ce501a441e401dc5bb0a2635d7
missingdays <daysaremissing@gmail.com>
2015-10-12 09:50:16 +0300

    Add new methodologies dir. Compare reduce in functional vs cycles in imperative

---
+++ methodologies/functional/reduce/python/reduce.py
@@ -0,0 +1,53 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+# vim:fenc=utf-8
+#
+# Copyright © 2015 missingdays <missingdays@missingdays>
+#
+# Distributed under terms of the MIT license.
+
+"""
+Comparison reduce in functional programming vs cycles in imperative
+"""
+
+# Imperative functions
+
+def imperative_sum(a):
+    s = 0
+    for elem in a:
+        s += elem
+    return s
+
+def imperative_mul(a):
+    m = 1
+    for elem in a:
+        m *= elem
+    return m
+
+# Functional
+
+def reduce(a, f, initial):
+    if len(a) == 0:
+        return initial
+    return reduce(a[1:], f, f(initial, a[0]))
+
+def functional_sum(a):
+    def sum(a, b):
+        return a + b
+    return reduce(a, sum, 0)
+
+def functional_mul(a):
+    def mul(a, b):
+        return a * b
+    return reduce(a, mul, 1)
+
+# Are they equal?
+
+a1 = [1, 2, 3, 4, 5]
+a2 = [-1, 0, 10, 15]
+
+print(imperative_sum(a1) == functional_sum(a1))
+print(imperative_sum(a2) == functional_sum(a2))
+
+print(imperative_mul(a1) == functional_mul(a1))
+print(imperative_mul(a2) == functional_mul(a2))
