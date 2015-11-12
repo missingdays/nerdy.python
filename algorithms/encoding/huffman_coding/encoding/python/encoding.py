ec714cc29da9a0e3c187b7d67dbf05cadb3fccb3
missingdays <rebovykin@gmail.com>
2015-11-12 19:42:41 +0300

    Add huffman decoding python

---
+++ algorithms/encoding/huffman_coding/encoding/python/encoding.py
@@ -0,0 +1,24 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+# vim:fenc=utf-8
+#
+# Copyright © 2015 missingdays <missingdays@missingdays>
+#
+# Distributed under terms of the MIT license.
+
+"""
+
+"""
+
+s = ''
+characters = {}
+
+for c in s:
+    if c in characters:
+        characters[c] += 1
+    else:
+        characters[c] = 1
+
+
+
+
