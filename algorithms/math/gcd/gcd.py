de0a56439d0c37aa5af20b1e3e31591376ff1455
missingdays <rebovykin@gmail.com>
2016-06-15 18:57:28 +0300

    Add gcd tests

--- algorithms/math/gcd/gcd.py
+++ algorithms/math/gcd/gcd.py
@@ -7,19 +7,15 @@
 
 '''
 
-#Finds greatest common division
 def gcd(a,b):
+    """
+    Finds greatest common divisor using Euclidean Algorithm
+    """
 
-    while a!=0 and b!=0:
-
+    while a != 0 and b != 0:
         if a > b:
             a = a % b
-
         else:
             b = b % a
 
-    return a + b
-
-print(gcd(20, 30)) #10
-print(gcd(15, 30)) #15
-print(gcd(480, 564)) #12
+    return max(a, b)
